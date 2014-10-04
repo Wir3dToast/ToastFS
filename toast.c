697507abe17521781db7da6c00921981914e33eb
Edward Kim <edbeta12@gmail.com>
2014-10-04 03:20:53 -0400

    Added file_system_type struct and basic mounting capabilites. Need to fill in superblock next.

--- toast.c
+++ toast.c
@@ -1,19 +1,56 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
+
+static struct file_system_type toast_type = {
+	.owner = THIS_MODULE,
+	.name = "ToastFS",
+	.mount = toast_mnt,
+	.kill_sb = toast_kill_sb,
+	.fs_flags = FS_REQUIRES_DEV, //Needs disk for operation
+
+};
+
+/*
+Comments:
+Mount_bdev initializes the superblock. Dentry contains the root of our system which toast_fill_sb creates
+*/
+static struct dentry *toast_mnt(struct file_system_type type,
+									int flags, char const *dev,
+									void *data)
+{
+	struct dentry *entry = mount_bdev(type, flags, dev, data,
+										toast_fill_super); 
+
+	if(IS_ERR(entry)) 
+		pr_err("Mounting ToastFS failed");
+	else 
+		printk("ToastFS Mounted Successfully")
+
+	return entry;	
+}
+
+static int toast_fill_super(struct super_block *sb, void *data, int silent) 
+{
+	/*Fill SuperBlock */	
+}
 
 static int __init toast_init(void) 
 {
-	printk("Hello World");
+
+	printk("ToastFS Loaded\n");
 	return 0;
 }
 
 static void __exit toast_exit(void)
 {
-	printk("Goodbye. Module unloaded");
+	printk("Goodbye. ToastFS Module unloaded\n");
 }
 
+
+
 module_init(toast_init);
-moudle_exit(toast_exit);
+module_exit(toast_exit);
 
 MODULE_LICENSE("MIT");
 MODULE_AUTHOR("Wir3dToast");
