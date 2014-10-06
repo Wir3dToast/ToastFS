b0313456fda71f89505c7e6a25aab874f780e912
Edward Kim <edbeta12@gmail.com>
2014-10-05 23:54:30 -0400

    Worked on filling superblock. Implementation of lookup and readdir is next. Compilation errors still intact. Not a compilable source file.

--- toast.c
+++ toast.c
@@ -2,7 +2,9 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 
-static struct file_system_type toast_type = {
+#include "toast.h"
+
+const static struct file_system_type toast_type = {
 	.owner = THIS_MODULE,
 	.name = "ToastFS",
 	.mount = toast_mnt,
@@ -11,11 +13,36 @@ static struct file_system_type toast_type = {
 
 };
 
+const static struct file_operations toast_dir_ops = {
+	.owner = THIS_MODULE,
+	.readdir = toast_readdir,
+};
+
+const static struct inode_operations toast_inode_ops = {
+	.lookup = toast_lookup_inode,
+}; 
+
+static int toast_readdir(struct file *filp, void *dirent, filldir_t filldir) 
+{
+	return 0;
+}
+
+/*
+Comments:
+Allows us to find names in directories.
+*/
+static dentry *toast_lookup_inode(struct inode *parent_inode,
+									struct dentry *child_dentry,
+									unsigned int flags)
+{
+	return NULL;
+}
+
 /*
 Comments:
 Mount_bdev initializes the superblock. Dentry contains the root of our system which toast_fill_sb creates
 */
-static struct dentry *toast_mnt(struct file_system_type type,
+static struct dentry *toast_mnt(struct file_system_type *type,
 									int flags, char const *dev,
 									void *data)
 {
@@ -25,26 +52,65 @@ static struct dentry *toast_mnt(struct file_system_type type,
 	if(IS_ERR(entry)) 
 		pr_err("Mounting ToastFS failed");
 	else 
-		printk("ToastFS Mounted Successfully")
+		printk("ToastFS Mounted Successfully");
 
 	return entry;	
 }
 
 static int toast_fill_super(struct super_block *sb, void *data, int silent) 
 {
-	/*Fill SuperBlock */	
+	struct inode *root;
+
+	sb->s_magic = TOAST_MAGIC_NUM;
+
+	root = new_inode(sb);
+
+	if(!root) {
+		pr_err("Inode Allocation Failed");
+		return -ENOMEM;
+	}
+
+	root->i_ino = get_next_ino(); //Get valid inode 
+	root->i_sb = sb;
+	root->i_op = &toast_inode_ops;
+	root->i_fop = &toast_dir_ops;
+	root->i_atime = root->i_mtime = root->i_ctime = CURRENT_TIME;
+	inode_init_owner(root,NULL,S_IFDIR);
+
+	sb->s_root = d_alloc_root(root);
+
+	if(!sb->s_root) {
+		pr_err("Root Dir Creation Failed");
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
+static void toast_kill_sb(struct super_block *sb)
+{
+
 }
 
 static int __init toast_init(void) 
 {
 
-	printk("ToastFS Loaded\n");
+	int regis = register_filesystem(&toast_type);
+	if(!regis) 
+		printk("ToastFS Loaded\n");
+	else
+		printk("ToastFS could not be registered. %d", ret);	
+
 	return 0;
 }
 
 static void __exit toast_exit(void)
 {
-	printk("Goodbye. ToastFS Module unloaded\n");
+	int regis = register_filesystem(&toast_type); 
+	if(!regis) 
+		printk("Goodbye. ToastFS Module unloaded\n");
+	else
+		printk("Cannot unregister ToastFS. %d", ret);
 }
 
 
---
+++ toast.h
@@ -0,0 +1,1 @@
+unsigned int TOAST_MAGIC_NUM = 0x746f6137
